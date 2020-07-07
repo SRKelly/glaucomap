b5512df07cea088c38fbc6a1ba014b903c0af15b
TSFPepper <67482646+TSFPepper@users.noreply.github.com>
2020-07-07 13:00:17 +0200

    Initial commit

---
+++ Creating_map.R
@@ -0,0 +1,3 @@
+#Glaucoma SES 2020
+library(rgdal)
+dat <- readOGR("C:\\Users\\Steph\\Documents\\PhD Related Files\\Yusrah Mapping")
---
+++ README.md
@@ -0,0 +1,2 @@
+# glaucoma-ses-england
+ Making a map of glaucoma demographics in England and Wales
